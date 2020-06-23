4620763b2a819f888287cfba24e645db2627c544
Craig Christopher Messi <craig.messi@gmail.com>
2020-06-23 19:43:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = (drivers) => {
-  return drivers.slice(0, 1)
-}
+const returnFirstTwoDrivers = (drivers) => (drivers.slice(0,2));
+const returnLastTwoDrivers = (drivers) => (drivers.slice(-2));
 
-const returnLastTwoDrivers = (drivers) => {
-  return drivers.slice(-2)
-}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = (multiplier) => ((fare) => (fare * multiplier));
 
-const createFareMultiplier = (multiplyer) => {
-  return function(number) {
-    return number * multiplyer
-  }
-}
+const fareDoubler = createFareMultiplier(2);
 
-const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3);
 
-const fareTripler = createFareMultiplier(3)
+let randomFunc = selectingDrivers[Math.round(Math.random())]
 
-const selectDifferentDrivers = (drivers, selectorFunction) => {
-  return selectorFunction(drivers)
-}
+const selectDifferentDrivers = (drivers, randomFunc) => (randomFunc(drivers))
