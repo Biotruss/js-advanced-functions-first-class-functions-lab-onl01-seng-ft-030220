0bc02824841f8954f4bce4ece22d6b764b303f57
Craig Christopher Messi <craig.messi@gmail.com>
2020-06-23 19:44:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = (drivers) => (drivers.slice(0,2));
-const returnLastTwoDrivers = (drivers) => (drivers.slice(-2));
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2)
+}
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
